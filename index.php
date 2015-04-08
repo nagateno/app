8f691a4a83e86dd117a1e32d7f02809d2e0299b8
nagateno <nagateno@gmail.com>
2015-04-08 10:43:20 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<body>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '343186345880675',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+
+    // ADD ADDITIONAL FACEBOOK CODE HERE
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+
 <form name="redirect">
 <center>
 <font face="Arial"><b>Загрузка ....<br><br>
